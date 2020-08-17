968e5051154ae9acfdf6d74fa50bd177cab8250d
David Kovacs <david.gergely.kovacs@regionh.dk>
2020-08-17 12:01:01 +0200

    created jaj_plot to easily visualize numpy data to check content of files. TODO: expand to version which created plots using ray_trace or other better quality figure

--- pythontoolkit/quick_plot.py
+++ pythontoolkit/quick_plot.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 '''
 Quickly plot numpy ND-array content
 '''
@@ -7,13 +8,13 @@ Quickly plot numpy ND-array content
 def jaj_plot(im, mapc = 0, lim = 0, alph = 0.3, axis = 1, grid = 1, colorbar = 0, colorbar_lab = ' '):
 	if lim == 0:
 		lim 	= im.min(), im.max()
-		print(lim)
+		#print('plot limits = ' + str(lim))
 	elif lim == "soft":
 		lim 	= -135, 215
-		print(lim)
+		#print('plot limits 2 = ' + str(lim))
 	if mapc == 0:
 		mapc 	= "gray"
-		print(mapc)
+		#print('colormap applied = ' + str(mapc))
 	fov = 500.
 	v = (0, round(((fov/512)*im.shape[1])), 0, round(((fov/512)*im.shape[0]))) #rescaling the axis
 	ax = plt.imshow(im, cmap = mapc, clim = lim, alpha = alph, extent = v)
